7b5505c2c99dc2ac34f8382c7abd2ac051decd79
chandra-mouli-k-s <chandramouli301@gmail.com>
2022-10-27 15:01:38 +0530

    spring boot application

---
+++ src/main/java/com/chan/mouli/createpost/CreatePostApplication.java
@@ -0,0 +1,13 @@
+package com.chan.mouli.createpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatePostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatePostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chan/mouli/createpost/CreatePostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chan.mouli.createpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatePostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
